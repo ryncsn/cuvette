aee082db182665044a4d7c7bacc59727b6c2dde2
Kairui Song <kasong@redhat.com>
2017-10-24 18:56:29 +0800

    Add keys folder and add whiteboard support

---
+++ cuvette/inspectors/meta.py
@@ -0,0 +1,34 @@
+"""
+Inspect a machine's CPU
+"""
+import logging
+
+from cuvette.inspectors.base import InspectorBase
+
+logger = logging.getLogger(__name__)
+
+
+class Inspector(InspectorBase):
+    """
+    Inspect machine's CPU
+    """
+    provide = {
+        "whiteboard": {
+            "description": "Additional string info for each machine.",
+            "type": str,
+        },
+    }
+
+    async def inspect(self: InspectorBase, machine, conn):
+        """
+        Do nothing
+        """
+        machine.setdefault('whiteboard', '')
+
+    def hard_filter(self, query: dict):
+        """
+        Filter nothing
+        """
+        return {}
+
+    soft_filter = hard_filter
